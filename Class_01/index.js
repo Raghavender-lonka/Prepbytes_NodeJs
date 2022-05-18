9b218da85c7ebbbb22e53387ab63450aaade0ec4
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-05-18 21:07:49 +0530

    html respone

--- Class_01/index.js
+++ Class_01/index.js
@@ -1,7 +1,8 @@
 const http = require("http") // http module
 
 const server = http.createServer((req, res) => {
-  res.write("Hello There")
+  // res.write("Hello There")
+  res.write("<h1>Hello there</h1>") //sending html response
   // res.write(JSON.stringify({ name:"Raghu", batch: 'DEC'}))
   res.end() // to end event loop
 })
