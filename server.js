7f2806bc964d42db3f0aa874490058cfc030cfc0
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-05-19 10:35:12 +0530

    node-02 with express

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const path = require("path")
+// const path = require("path")
 
 const express = require("express") // it will load the express package in our app
 
@@ -11,53 +11,14 @@ const app = express()
 
 // home route
 app.get("/", (req, res, next) => {
-  //res.send()
-  //res.sendFile()
-  //res.render()
-  //res.json()
-  res.send("Hi") //text
-  //   res.send("<h1>Hello there</h1>") //html
-  //json data
-  //   res.send({
-  //     name: "raghu",
-  //     id: 1,
-  //   })
+  res.send("Hello there") //text
 })
 
-//user route
-// app.get("/user", (req, res, next) => {
-//   // two different kinds of get request parameters
-
-//   //1. query param
-//   //2. path param
-//   // console.log(req.query)
-//   // http://localhost:8000/user?name=rag&age=23&address=gadwal
-//   // const name = req.query.name
-//   // const age = req.query.age
-//   // const address = req.query.address
-
-//   // res.send(`${name}, ${age}, ${address}`)
-//   // res.send("User") //text
-// })
-
-// path param
-
-// app.get("/user/:data?/:id", function (req, res) {
-//   // console.log(req.params.data) // data is not keyword, but in routes we had given /:data, so we have to match that
-
-//   //question mark (?) in route means it is optional
-//   res.send(req.params.data + " " + req.params.id)
-// })
-
-app.get("/html", (req, res) => {
-  // console.log(__dirname) //C:\Users\raghu\Prepbytes-NodeJs\Test
-  // console.log(process.argv)
-  const filePath = path.join(__dirname + "/views/home.html") // absoluate path of file
-  res.sendFile(filePath)
-})
-
-app.post("/postroute", (req, res) => {
-  res.json("this")
+app.get("/api/main", (req, res, next) => {
+  res.write("<h1>Express.Js</h1>")
+  res.write(
+    "<p>Express  is a light-weight web application framework to help organize your web application into an MVC architecture on the server side. You can then use a database like MongoDB  to provide a backend for your Node.js application. Express.js basically helps you manage everything, from routes, to handling requests and views.</p>"
+  )
 })
 
 app.listen(port)
