8babf88465e37384a192d4234820257cc5191ceb
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-05-19 19:20:28 +0530

    changes

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const app = express()
 // route creation
 
 // home route
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
   res.send("Hello there") //text
 })
 
-app.get("/api/main", (req, res, next) => {
+app.get("/api/main", (req, res) => {
   res.write("<h1>Express.Js</h1>")
   res.write(
     "<p>Express  is a light-weight web application framework to help organize your web application into an MVC architecture on the server side. You can then use a database like MongoDB  to provide a backend for your Node.js application. Express.js basically helps you manage everything, from routes, to handling requests and views.</p>"
