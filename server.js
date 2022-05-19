ce3d2c4115b41f892a91341779b771184e17df41
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-05-19 10:38:58 +0530

    server end

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get("/api/main", (req, res, next) => {
   res.write(
     "<p>Express  is a light-weight web application framework to help organize your web application into an MVC architecture on the server side. You can then use a database like MongoDB  to provide a backend for your Node.js application. Express.js basically helps you manage everything, from routes, to handling requests and views.</p>"
   )
+  res.end()
 })
 
 app.listen(port)
